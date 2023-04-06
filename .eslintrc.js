57981882ff6108d60bdad015109c8d3d7d22d9f1
yunkooo <yunkoooooo@gmail.com>
2023-04-07 00:39:57 +0900

    init: 프로젝트 초기 설정

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+
+  extends: ['react-app', 'naver', 'eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['react'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  rules: {
+    'import/prefer-default-export': 0,
+    'no-console': 2,
+    'no-unused-vars': 'warn',
+    'object-curly-newline': 1,
+  },
+};
