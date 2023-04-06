66aa06cf403844ee296a67eaf4dec9b1190f2d7d
yunkooo <yunkoooooo@gmail.com>
2023-04-07 01:30:10 +0900

    design: charkra provider 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './lib/styles/theme.js';
 
 function App() {
-  return <div className='app'></div>;
+  return (
+    <ChakraProvider theme={theme}>
+      <div className='app'></div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
