146c1851ed9f885fd1e78c3034ff21f84e707f63
yunkooo <yunkoooooo@gmail.com>
2023-04-08 05:22:31 +0900

    feat: SignIn component 마크업, 경로 오류 수정

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,20 @@
+import { Box, Heading, Stack, Link } from '@chakra-ui/react';
+import LoginForm from '../../components/LoginForm';
+
 const SignIn = () => {
-  return <div>Sign In</div>;
+  return (
+    <Box w='sm' rounded={'lg'} bg={'gray.50'} py={8} px={10} textAlign={'center'}>
+      <Heading fontSize={'3xl'} mb={6}>
+        Login
+      </Heading>
+      <Stack spacing={6}>
+        <LoginForm />
+        <Link color={'gray'} _hover={{ color: 'black' }}>
+          Sign in
+        </Link>
+      </Stack>
+    </Box>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,32 +1,5 @@
-import { Box, FormControl, FormLabel, Heading, Input, Stack, Link } from '@chakra-ui/react';
-import CommonButton from '../../components/common/Button';
-
 const SignUp = () => {
-  return (
-    <Box w='sm' rounded={'lg'} bg={'gray.50'} py={8} px={10} textAlign={'center'}>
-      <Heading fontSize={'3xl'} mb={6}>
-        Login
-      </Heading>
-      <Stack spacing={6}>
-        <form>
-          <Stack spacing={3}>
-            <FormControl>
-              <FormLabel>Email</FormLabel>
-              <Input type='email' />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Password</FormLabel>
-              <Input type='password' />
-            </FormControl>
-            <CommonButton children={'로그인'} bg={'blue.400'} color={'white'} _hover={{ bg: 'blue.500' }} />
-          </Stack>
-        </form>
-        <Link color={'gray'} _hover={{ color: 'black' }}>
-          Sign in
-        </Link>
-      </Stack>
-    </Box>
-  );
+  return <div>SignUp</div>;
 };
 
 export default SignUp;
