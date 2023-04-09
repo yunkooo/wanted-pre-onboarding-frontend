5580e4d892c7d53223b4701a1c26db94f42ac125
yunkooo <yunkoooooo@gmail.com>
2023-04-09 09:52:36 +0900

    feat: 회원가입 유효성 검사 기능 구현

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -2,36 +2,60 @@ import React from 'react';
 import { Stack } from '@chakra-ui/react';
 import CommonButton from '../common/CommonButton';
 import CommonInput from '../common/CommonInput';
+import useInput from '../../lib/hook/useInput';
+import { validation } from '../../lib/utils/validation';
 
 const RegisterForm = () => {
+  const [{ email, password, rePassword }, onChage] = useInput({ email: '', password: '', rePassword: '' });
+
+  const isDisabled =
+    !!email &&
+    !!password &&
+    !!rePassword &&
+    validation.isEmailCheck(email).value &&
+    validation.isPasswordCheck(password).value &&
+    validation.isPasswordReCheck(password, rePassword).value;
+
   return (
     <form>
       <Stack spacing={3}>
         <CommonInput
           id='email'
           type='email'
+          name='email'
           placeholder='이메일 입력'
           label='Email'
           testid='email-input'
-          errorMSG={''}
+          onChange={onChage}
+          errorMSG={validation.isEmailCheck(email).message}
         />
         <CommonInput
           id='password'
           type='password'
+          name='password'
           placeholder='비밀번호 입력'
           label='Password'
           testid='password-input'
-          errorMSG={''}
+          onChange={onChage}
+          errorMSG={validation.isPasswordCheck(password).message}
         />
         <CommonInput
           id='passwordCheck'
           type='password'
+          name='rePassword'
           placeholder='비밀번호 확인 입력'
           label='Password check'
-          errorMSG={''}
+          onChange={onChage}
+          errorMSG={validation.isPasswordReCheck(password, rePassword).message}
         />
 
-        <CommonButton children={'회원가입'} bg={'blue.400'} color={'white'} _hover={{ bg: 'blue.500' }} />
+        <CommonButton
+          children={'회원가입'}
+          bg={'blue.400'}
+          color={'white'}
+          _hover={{ bg: 'blue.500' }}
+          isDisabled={!isDisabled}
+        />
       </Stack>
     </form>
   );
