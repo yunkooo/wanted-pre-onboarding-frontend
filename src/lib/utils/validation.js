8a77954949a67323b7fe1e1b24108a450e8a2029
yunkooo <yunkoooooo@gmail.com>
2023-04-09 15:12:23 +0900

    fix: 로직 수정으로 인한 함수 제거

--- src/lib/utils/validation.js
+++ src/lib/utils/validation.js
@@ -24,22 +24,9 @@ export const validation = {
       message: isCheck ? '' : validationError.PASSWORD_ERROR,
     };
   },
-  isPasswordReCheck: (password, rePassword) => {
-    let isCheck = true;
-
-    if (rePassword === '') {
-      isCheck = true;
-    } else {
-      isCheck = password === rePassword;
-    }
-    return {
-      value: isCheck,
-      message: isCheck ? '' : validationError.PASSWORD_CHECK_ERROR,
-    };
-  },
 };
 
-const validationError = {
+export const validationError = {
   EMAIL_ERROR: '@를 포함하여 입력해주세요.',
   PASSWORD_ERROR: '8자리 이상 입력해주세요.',
   PASSWORD_CHECK_ERROR: '패스워드와 동일하게 입력해주세요.',
