75758fdc86d1a7e7c5b1030f5f7356054860d608
yunkooo <yunkoooooo@gmail.com>
2023-04-09 19:54:25 +0900

    design: Todo 페이지 UI 마크업

--- src/pages/Todo/index.jsx
+++ src/pages/Todo/index.jsx
@@ -1,5 +1,24 @@
+import { Box, Heading, Stack, Icon, Button } from '@chakra-ui/react';
+import { GrLogout } from 'react-icons/gr';
+import TodoInput from '../../components/TodoIput';
+import TodoList from '../../components/TodoList';
+
 const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <Box position={'relative'} w='sm' rounded={'lg'} bg={'gray.50'} py={8} px={10} textAlign={'center'}>
+      <Heading fontSize={'3xl'} mb={4}>
+        Todo List
+      </Heading>
+
+      <Stack mb={1} align={'flex-end'}>
+        <Button bg={'inherit'} position={'absolute'} top={'40px'} boxSize={'6'}>
+          <Icon as={GrLogout} />
+        </Button>
+      </Stack>
+      <TodoInput />
+      <TodoList />
+    </Box>
+  );
 };
 
 export default Todo;
