d06f50f145b8a98cea3e4d443437dcc28cfd37ce
yunkooo <yunkoooooo@gmail.com>
2023-04-09 09:42:32 +0900

    feat: 유효성 검사를 위한 validation 구현

--- src/lib/utils/validation.js
+++ src/lib/utils/validation.js
@@ -1,9 +1,46 @@
-const validation = {
+export const validation = {
   isEmailCheck: email => {
-    return email.includes('@');
+    let isCheck = true;
+
+    if (email === '') {
+      isCheck = true;
+    } else {
+      isCheck = email.includes('@');
+    }
+
+    return { value: isCheck, message: isCheck ? '' : validationError.EMAIL_ERROR };
   },
   isPasswordCheck: password => {
-    if (password.length >= 8) return true;
-    else return false;
+    let isCheck = true;
+
+    if (password === '') {
+      isCheck = true;
+    } else {
+      isCheck = password.length >= 8;
+    }
+
+    return {
+      value: isCheck,
+      message: isCheck ? '' : validationError.PASSWORD_ERROR,
+    };
   },
+  isPasswordReCheck: (password, rePassword) => {
+    let isCheck = true;
+
+    if (rePassword === '') {
+      isCheck = true;
+    } else {
+      isCheck = password === rePassword;
+    }
+    return {
+      value: isCheck,
+      message: isCheck ? '' : validationError.PASSWORD_CHECK_ERROR,
+    };
+  },
+};
+
+const validationError = {
+  EMAIL_ERROR: '@를 포함하여 입력해주세요.',
+  PASSWORD_ERROR: '8자리 이상 입력해주세요.',
+  PASSWORD_CHECK_ERROR: '패스워드와 동일하게 입력해주세요.',
 };
