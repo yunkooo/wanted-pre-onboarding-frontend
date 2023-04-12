f4793f487a9c76026bd67b336a9433c46585ce75
yunkooo <yunkoooooo@gmail.com>
2023-04-13 02:51:32 +0900

    feat: checkBox 기능 추가

--- src/components/todo/TodoItem/index.jsx
+++ src/components/todo/TodoItem/index.jsx
@@ -2,10 +2,24 @@ import React, { useState } from 'react';
 import { Stack, ListItem, Input, Button, Text, Checkbox } from '@chakra-ui/react';
 import { EditIcon, CheckIcon, CloseIcon } from '@chakra-ui/icons';
 import DeleteBtn from '../buttons/DeleteBtn';
+import { updateApi } from '../../../api/todo';
+import { useTodoContext } from '../../../context/todoContext';
 
 export default function TodoItem({ todoData }) {
+  const { id, isCompleted, todo } = todoData;
+  const { updateTodo } = useTodoContext();
+
   const [isModify, setIsModify] = useState(false);
-  const [isCheck, setIsCheck] = useState(false);
+  const [isCheck, setIsCheck] = useState(isCompleted);
+
+  const checkedHandler = async () => {
+    setIsCheck(prev => !isCheck);
+    const res = await updateApi({ ...todoData, isCompleted: !isCheck });
+
+    if (res.status === 200) {
+      updateTodo(res.data);
+    }
+  };
 
   return (
     <>
@@ -31,22 +45,22 @@ export default function TodoItem({ todoData }) {
         <ListItem listStyleType={'none'}>
           <Stack flexDirection={'row'} align={'center'} justify='space-between'>
             <Checkbox
-              isChecked={todoData.isCompleted}
+              isChecked={isCheck}
               onChange={() => {
-                setIsCheck(!isCheck);
+                checkedHandler();
               }}
               colorScheme='green'
               mt={'0.5rem'}
               w={'200px'}
             >
               <Text as='b' mt={'0.5rem'} textAlign={'left'}>
-                {todoData.todo}
+                {todo}
               </Text>
             </Checkbox>
             <Button mt={0} bg={'inherit'} w={'min-content'} onClick={() => setIsModify(!isModify)}>
               <EditIcon />
             </Button>
-            <DeleteBtn todoId={todoData.id} />
+            <DeleteBtn todoId={id} />
           </Stack>
         </ListItem>
       )}
